46101b1d11966bae9e2bbe0d1797f3aa0df81950
ZYTK Devs <smcclellan0525@outlook.com>
2022-04-27 09:21:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -182,7 +182,6 @@ function startGame() {
   if (ready) {
     ready = false;
     scoreElement.innerText = 0;
-    buttonsElement.style.opacity = 1;
     instructionsElement.style.opacity = 0;
     renderer.setAnimationLoop(animation);
   }
